9750d2c00e0fb749180949a5e2bd5466b661ddae
Jhoon Saravia <jhoonsar@gmail.com>
2015-07-10 11:53:24 -0500

    Created fragment for player

---
+++ app/src/main/java/pe/jota/spotifystreamer/PlayerActivity.java
@@ -0,0 +1,15 @@
+package pe.jota.spotifystreamer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
---
+++ app/src/main/java/pe/jota/spotifystreamer/PlayerActivityFragment.java
@@ -0,0 +1,23 @@
+package pe.jota.spotifystreamer;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class PlayerActivityFragment extends DialogFragment {
+
+    public PlayerActivityFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_player, container, false);
+    }
+}
